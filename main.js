4defb286e0474606a93c0ac61b141909056da9d6
bandstrar <rob.bandstra@gmail.com>
2020-07-23 19:24:29 -0500

    added array of objects

--- main.js
+++ main.js
@@ -1,1 +1,46 @@
-console.log("Greyhounds > other animals")
+"use strict";
+
+const pets = [
+    {
+        "image": "./images/cruisin.jpg",
+        "name": "Cruisin",
+        "color": "Brindle",
+        "specialSkill": "Super chill and loves to relax",
+        "typeOfPet": "Dog"
+      },
+    {
+        "image": "./images/easy.jpg",
+        "name": "Easy",
+        "color": "Black",
+        "specialSkill": "Gives lots of snoots and boops",
+        "typeOfPet": "Dog"
+      },
+    {
+        "image": "./images/black-snake.jpg",
+        "name": "Mambo",
+        "color": "Black",
+        "specialSkill": "Can coil into a very small ball. A smolbol.",
+        "typeOfPet": "Snake"
+      },
+    {
+        "image": "./images/cornsnake.jpg",
+        "name": "Kaa",
+        "color": "Orange",
+        "specialSkill": "Can hypnotize wolf cubs",
+        "typeOfPet": "Snake"
+      },
+    {
+        "image": "./images/white-cat.jpg",
+        "name": "Duchess",
+        "color": "White",
+        "specialSkill": "Can lead a boisterous sing-a-long",
+        "typeOfPet": "Cat"
+      },
+    {
+        "image": "./images/orange-cat.jpg",
+        "name": "Oliver",
+        "color": "Orange",
+        "specialSkill": "Is friends with Billy Crystal",
+        "typeOfPet": "Cat"
+      }
+]
